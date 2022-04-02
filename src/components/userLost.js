d7cba43f338e591b7e71d57688692bc297da060c
Nicola <nicola.mcfadden@dcmail.ca>
2022-04-02 10:58:09 -0400

    Added case components for user winning vs losing

---
+++ src/components/userLost.js
@@ -0,0 +1,11 @@
+import '../styles.css';
+
+const UserLost = () => {
+    return (
+        <div>
+            <h1>Sorry, you lost</h1>
+        </div>
+    )
+};
+
+export default UserLost;
---
+++ src/components/userWon.js
@@ -0,0 +1,11 @@
+import '../styles.css';
+
+const UserWon = () => {
+    return (
+        <div>
+            <h1>Congrats! You Won</h1>
+        </div>
+    )
+};
+
+export default UserWon;
