e80ec997de5d0e65cce2a562111559c2bbcf30f3
Nicola <nicola.mcfadden@dcmail.ca>
2022-04-02 10:57:43 -0400

    Updated contest page

--- src/pages/contestPage.js
+++ src/pages/contestPage.js
@@ -1,11 +1,16 @@
 import '../styles.css';
 import ScratchCard from "../components/scratchCard";
+import React from 'react';
+import ContestHeader from '../components/contestHeader';
+import Sponsors from '../components/sponsors';
 
 const ContestPage = () => {
     return (
         <main>
+            <ContestHeader />
             <h1>I am the contest page 😎</h1>
             <ScratchCard />
+            <Sponsors />
         </main>
     );
 }
