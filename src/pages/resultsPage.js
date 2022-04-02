f866d2db4fc40083226ae017c0aef4a968face16
Nicola <nicola.mcfadden@dcmail.ca>
2022-04-02 10:57:54 -0400

    Updated results page

--- src/pages/resultsPage.js
+++ src/pages/resultsPage.js
@@ -1,13 +1,35 @@
 import '../styles.css';
 import ScratchCard from "../components/scratchCard";
+import React from 'react';
+import UserWon from "../components/userWon";
+import UserLost from "../components/userLost";
+import ContestHeader from '../components/contestHeader';
+import Sponsors from '../components/sponsors';
 
-const ResultsPage = () => {
-    return (
-        <main>
-            <h1>I am the results page 🥳</h1>
-            <ScratchCard />
-        </main>
-    );
+class ResultsPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            win: false
+        };
+    }
+
+    render() {
+        const didUserWin = this.state.win;
+        return (
+            <main>
+                <ContestHeader />
+                <h1>I am the results page 🥳</h1>
+                <ScratchCard />
+                {didUserWin
+                    ? <UserWon />
+                    : <UserLost />
+                }
+                <Sponsors />
+            </main>
+        );
+    }
 }
 
 export default ResultsPage;
