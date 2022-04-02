372f595b5d57ba99fb01acb743bb881a83276141
Nicola <nicola.mcfadden@dcmail.ca>
2022-04-02 10:57:24 -0400

    Created component for sponsors box

---
+++ src/components/sponsors.js
@@ -0,0 +1,17 @@
+import '../styles.css';
+
+const Sponsors = () => {
+    return (
+        <article>
+            <h3>Check Out Our Sponsors</h3>
+            <div className='sponsors-container'>
+                <img src={require('../images/png/logo-taco-superstore.png')} />
+                <img src={require('../images/png/logo-raw-cabbage-on-a-stick-hut.png')} />
+                <img src={require('../images/png/logo-glorbotronic-burgers.png')} />
+                <img src={require('../images/png/logo-fresh-kicks.png')} />
+            </div>
+        </article>
+    )
+};
+
+export default Sponsors;
