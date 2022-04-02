857a0f79e7e70a139cbd3c0ea8c801c7d7e29fde
Nicola <nicola.mcfadden@dcmail.ca>
2022-04-02 11:11:49 -0400

    Fixed syntax error

--- src/components/userWon.js
+++ src/components/userWon.js
@@ -7,7 +7,7 @@ const UserWon = () => {
             <aside className='skill-testing'>
                 <p>Please answer the simple skill testing question to claim your prize</p>
                 <p>
-                    <label for='math'>2 + 2 =</label>
+                    <label htmlFor='math'>2 + 2 =</label>
                     <input type='number' name='math' id='math'></input>
                 </p>
                 <input type='submit' className='submit' id='submit' value='Submit'></input>
