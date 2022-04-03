9782c3f42cf02f94b7b47c77b1eefd3bdc3ac3c3
Jeriel Benavides <jeriel.benavides@dcmail.ca>
2022-04-03 03:06:26 -0400

    Css styles for card

--- src/components/scratchableCard.js
+++ src/components/scratchableCard.js
@@ -15,13 +15,7 @@ const ScratchableCard = () => {
                 <ScratchCard width={300} height={300} cover={couponCover}>
                     <form className="form" >
                         <h2>Hello There!</h2>
-                        <h1><code>Coupon code : 1651613335</code></h1>
-                        <div>
-                            <input type="text" name="code" placeholder="Coupon Code"></input>
-                        </div>
-                        <div>
-                            <input type="submit" value="Submit"></input>
-                        </div>
+                        <p>I am a hidden card</p>
                     </form>
                 </ScratchCard>
         </article>
