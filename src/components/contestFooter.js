abd25fc98a4764dccfc9ec68c82c825cb33aa524
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-04-03 15:56:00 -0400

    Changed the layout so its fully responsive at all sizes.

--- src/components/contestFooter.js
+++ src/components/contestFooter.js
@@ -5,17 +5,17 @@ import '../styles.css';
 const ContestFooter = () => {
     return (
         <section id='nav-buttons-footer' className='grid-x footer'>
-            <article  className='cell large-2 medium-4 small-12 test'>
+            <article  className='cell large-2 medium-4 small-12'>
                 <Link to="/legal"><input type="button" value="Terms and Conditions"></input></Link>
             </article>
     
-            <article className='cell large-2 medium-4 small-12'> 
-                <p>Place holder for Sign In Link</p>
+            <article className='cell large-3 medium-4 small-12'> 
+            <Link to="/form"><input type="button" value="Sign Up" id=""></input></Link>
             </article>
             
-            <article className='cell large-6 medium-2 small-12'></article>
+            <article className='cell large-5 medium-2 small-12'></article>
 
-            <article className='cell large-2 medium-4 small-12'>
+            <article className='cell large-2 medium-2 small-12'>
                 <i className="fa-brands fa-twitter"></i>
                 <i className="fa-brands fa-youtube"></i>
                 <i className="fa-brands fa-instagram"></i>
