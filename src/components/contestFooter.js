94d11243589b153231d49389566dbdd32f3a1c27
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-04-02 22:52:50 -0400

    Updated the footer component and the styling

--- src/components/contestFooter.js
+++ src/components/contestFooter.js
@@ -1,11 +1,27 @@
+import { Link } from 'react-router-dom';
+
 import '../styles.css';
 
 const ContestFooter = () => {
     return (
-        <article>
-            <h1>Contest Footer</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-        </article>
+        <section id='nav-buttons-footer' className='grid-x footer'>
+            <article  className='cell large-2 medium-4 small-12 test'>
+                <Link to="/legal"><input type="button" value="Terms and Conditions"></input></Link>
+            </article>
+    
+            <article className='cell large-2 medium-4 small-12'> 
+                <p>Place holder for Sign In Link</p>
+            </article>
+            
+            <article className='cell large-6 medium-2 small-12'></article>
+
+            <article className='cell large-2 medium-4 small-12'>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-youtube"></i>
+                <i class="fa-brands fa-instagram"></i>
+            </article>
+
+        </section>
     )
 };
 
