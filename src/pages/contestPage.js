eab607b3690af6eaffe24f3025bbe01f029c6e0b
Jeriel Benavides <jeriel.benavides@dcmail.ca>
2022-04-04 14:10:13 -0400

    Calculating hours

--- src/pages/contestPage.js
+++ src/pages/contestPage.js
@@ -9,9 +9,13 @@ import Sponsors from '../components/sponsors';
 const ContestPage = (props) => {
     let navigate = useNavigate();
     useEffect(() => {
+        console.log(props.user);
         if(props.user===0){
             navigate("/form", { replace: true });
         }
+        //Calculate hours elapsed since last time....
+        let hours = Math.abs(props.user['lastParticipated'] - new Date()) / 36e5;
+        console.log(hours);
     });
 
     return (
