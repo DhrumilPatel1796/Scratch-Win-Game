57fa3e8fa52eeae538eddb56cc0ef76ff976473d
Jeriel Benavides <jeriel.benavides@dcmail.ca>
2022-04-04 20:56:00 -0400

    header styles

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,53 +12,54 @@ const Navbar = () => {
         }
   return (
     <>
-
-      <div className="title-bar" data-responsive-toggle="example-animated-menu">
-        <button className="menu-icon" type="button" onClick={toggleNav} data-toggle="" data-hide-for="medium"></button>
-        <div className="title-bar-title">
-          <Link to="/">
-            <img
-              className="bmd-logo"
-              src={require("../images/png/logo.png")}
-            />
-          </Link>
+    <main className="grid-x grid-padding-x">
+      <div className="cell large-10 medium-10 small-12">
+        <div className="title-bar" data-responsive-toggle="example-animated-menu">
+              <button className="menu-icon" type="button" onClick={toggleNav} data-toggle="" data-hide-for="medium"></button>
+              <div className="title-bar-title">
+                <Link to="/">
+                  <img
+                    className="bmd-logo"
+                    src={require("../images/png/logo.png")}
+                  />
+                </Link>
+              </div>
+        </div>
+        <div className="top-bar" id="example-animated-menu" data-animate="hinge-in-from-top spin-out">
+      <div className="top-bar-left">
+      <Link to="/">
+          <img
+            className="bmd-logo hide-for-medium-only hide-for-small-only"
+            src={require("../images/png/logo.png")}
+          />
+        </Link>
+        {(toggleMenu) && (
+          <nav id="nav-buttons">
+            <Link to="/form">
+              <input type="button" value="Sign Up" id=""></input>
+            </Link>
+            <Link to="/legal">
+              <input type="button" value="Terms and Conditions" id=""></input>
+            </Link>
+            <Link to="/results">
+              <input type="button" value="Results" id=""></input>
+            </Link>
+            <Link to={contestPageLink}>
+              <input
+                type="button"
+                value="Play To Win"
+                id="play"
+                className="button"
+              ></input>
+            </Link>
+          </nav>
+          )}
+      </div>
         </div>
       </div>
-
-<div className="top-bar" id="example-animated-menu" data-animate="hinge-in-from-top spin-out">
-  <div className="top-bar-left">
-  <Link to="/">
-      <img
-        className="bmd-logo hide-for-medium-only hide-for-small-only"
-        src={require("../images/png/logo.png")}
-      />
-    </Link>
-    {(toggleMenu) && (
-      <nav id="nav-buttons">
-        <Link to="/form">
-          <input type="button" value="Sign Up" id="" onClick={toggleNav}></input>
-        </Link>
-        <Link to="/legal">
-          <input type="button" value="Terms and Conditions" id="" onClick={toggleNav}></input>
-        </Link>
-        <Link to="/results">
-          <input type="button" value="Results" id="" onClick={toggleNav}></input>
-        </Link>
-        <Link to={contestPageLink} onClick={toggleNav}>
-          <input
-            type="button"
-            value="Play To Win"
-            id="play"
-            className="button"
-          ></input>
-        </Link>
-      </nav>
-      )}
-  </div>
-</div>
-
-      <Outlet />
-    </>
+    </main>
+<Outlet />
+  </>
   );
 };
 
