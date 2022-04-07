837e7f4790142d0ed492b4caa54664f86a9783db
Aleisha Silvera <aleishasilvera@me.com>
2022-04-07 15:29:22 -0400

    fixed header and footer alignments

--- src/components/contestFooter.js
+++ src/components/contestFooter.js
@@ -1,28 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import '../styles.css';
+import "../styles.css";
 
 const ContestFooter = () => {
-    return (
-        <section id='nav-buttons-footer' className='grid-x footer'>
-            <article  className='cell large-2 medium-4 small-12'>
-                <Link to="/legal"><input type="button" value="Terms and Conditions"></input></Link>
-            </article>
-    
-            <article className='cell large-3 medium-4 small-12'> 
-            <Link to="/form"><input type="button" value="Sign Up" id=""></input></Link>
-            </article>
-            
-            <article className='cell large-5 medium-2 small-12'></article>
+  return (
+    <footer id="nav-buttons-footer" className="grid-container full">
+      <div className="grid-x grid-padding-x footer">
+        <div className="cell large-2 medium-4 small-12">
+          <Link to="/legal">
+            <input type="button" value="Terms and Conditions"></input>
+          </Link>
+        </div>
 
-            <article className='cell large-2 medium-2 small-12'>
-                <i className="fa-brands fa-twitter"></i>
-                <i className="fa-brands fa-youtube"></i>
-                <i className="fa-brands fa-instagram"></i>
-            </article>
+        <div className="cell large-2 medium-4 small-12">
+          <Link to="/form">
+            <input type="button" value="Sign Up" id=""></input>
+          </Link>
+        </div>
 
-        </section>
-    )
+        <div className="cell large-offset-5 medium-2 small-12">
+          <i className="fa-brands fa-twitter"></i>
+          <i className="fa-brands fa-youtube"></i>
+          <i className="fa-brands fa-instagram"></i>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
-export default ContestFooter
+export default ContestFooter;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,65 +1,82 @@
 import { Outlet, Link } from "react-router-dom";
-import React, { useCallback, useState, useEffect, useRef } from 'react';
+import React, { useCallback, useState, useEffect, useRef } from "react";
 
 const Navbar = () => {
-  const contestPageLink = { 
-    pathname: "/contest", 
-    stringTest: "Hello from parent" 
+  const contestPageLink = {
+    pathname: "/contest",
+    stringTest: "Hello from parent",
   };
   const [toggleMenu, setToggleMenu] = useState(true);
-    const toggleNav = () => {
-        setToggleMenu(!toggleMenu)
-        }
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+  };
   return (
     <>
-    <main className="grid-x grid-padding-x">
-      <div className="cell large-10 medium-10 small-12">
-        <div className="title-bar" data-responsive-toggle="example-animated-menu">
-              <button className="menu-icon" type="button" onClick={toggleNav} data-toggle="" data-hide-for="medium"></button>
-              <div className="title-bar-title">
-                <Link to="/">
-                  <img
-                    className="bmd-logo"
-                    src={require("../images/png/logo.png")}
-                  />
-                </Link>
-              </div>
+      <main className="grid-container navbar">
+        <div className="cell large-11 medium-11 small-12">
+          <div
+            className="title-bar"
+            data-responsive-toggle="example-animated-menu"
+          >
+            <button
+              className="menu-icon"
+              type="button"
+              onClick={toggleNav}
+              data-toggle=""
+              data-hide-for="medium"
+            ></button>
+            <div className="title-bar-title">
+              <Link to="/">
+                <img
+                  className="bmd-logo"
+                  src={require("../images/png/logo.png")}
+                />
+              </Link>
+            </div>
+          </div>
+          <div
+            className="top-bar"
+            id="example-animated-menu"
+            data-animate="hinge-in-from-top spin-out"
+          >
+            <div className="top-bar-left">
+              <Link to="/">
+                <img
+                  className="bmd-logo hide-for-medium-only hide-for-small-only"
+                  src={require("../images/png/logo.png")}
+                />
+              </Link>
+              {toggleMenu && (
+                <nav id="nav-buttons">
+                  <Link to="/form">
+                    <input type="button" value="Sign Up" id=""></input>
+                  </Link>
+                  <Link to="/legal">
+                    <input
+                      type="button"
+                      value="Terms and Conditions"
+                      id=""
+                    ></input>
+                  </Link>
+                  <Link to="/results">
+                    <input type="button" value="Results" id=""></input>
+                  </Link>
+                  <Link to={contestPageLink}>
+                    <input
+                      type="button"
+                      value="Play To Win"
+                      id="play"
+                      className="button"
+                    ></input>
+                  </Link>
+                </nav>
+              )}
+            </div>
+          </div>
         </div>
-        <div className="top-bar" id="example-animated-menu" data-animate="hinge-in-from-top spin-out">
-      <div className="top-bar-left">
-      <Link to="/">
-          <img
-            className="bmd-logo hide-for-medium-only hide-for-small-only"
-            src={require("../images/png/logo.png")}
-          />
-        </Link>
-        {(toggleMenu) && (
-          <nav id="nav-buttons">
-            <Link to="/form">
-              <input type="button" value="Sign Up" id=""></input>
-            </Link>
-            <Link to="/legal">
-              <input type="button" value="Terms and Conditions" id=""></input>
-            </Link>
-            <Link to="/results">
-              <input type="button" value="Results" id=""></input>
-            </Link>
-            <Link to={contestPageLink}>
-              <input
-                type="button"
-                value="Play To Win"
-                id="play"
-                className="button"
-              ></input>
-            </Link>
-          </nav>
-          )}
-      </div>
-        </div>
-      </div>
-    </main>
-<Outlet />
-  </>
+      </main>
+      <Outlet />
+    </>
   );
 };
 
